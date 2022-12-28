423e536224ff009f441bdc48cc422986a7eb7511
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2022-12-28 17:59:54 +0700

    Add method insert to insert element

--- BinarySearchTree_050/Program.cs
+++ BinarySearchTree_050/Program.cs
@@ -30,6 +30,47 @@ namespace BinarySearchTree_050
         {
             ROOT = null; /*Initializing ROOT to null*/
         }
+        public void search(string element, ref Node parent, ref Node currentNode)
+        {
+            /*This function search the currentNode of the specified Node as
+             well as the current Node of its parents*/
+            currentNode = ROOT;
+            parent = null;
+            while ((currentNode != null) && (currentNode.info != element))
+            {
+                parent = currentNode;
+                if (string.Compare(element, currentNode.info) < 0)
+                    currentNode = currentNode.leftchild;
+                else
+                    currentNode = currentNode.rightchild;
+            }
+        }
+        public void insert(string element)/*insert a node in the binary search tree*/
+        {
+            Node tmp, parent = null, currentNode = null;
+            search(element, ref parent, ref currentNode);
+            if (currentNode != null)/*Check if the Node to be inserted already inserted or not*/
+            {
+                Console.WriteLine("Duplicate words not allowed");
+                return;
+            }
+            else/*If the specified nod is not present*/
+            {
+                tmp = new Node(element, null, null);/*Creates a Node*/
+                if (parent == null)/*If the tree is empty*/
+                {
+                    ROOT = tmp;
+                }
+                else if (string.Compare(element, parent.info) < 0)
+                {
+                    parent.leftchild = tmp;
+                }
+                else
+                {
+                    parent.rightchild = tmp;
+                }
+            }
+        }
         
     }
 }
